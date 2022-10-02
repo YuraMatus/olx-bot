e506bcf113df182337077193b0ccb9913b4aca6d
yurii.matus <yurii.matus@sigma.software>
2022-10-02 15:56:33 +0300

    fix scheduler

--- src/main/java/olx/rent/bot/olx/UpdateScheduler.java
+++ src/main/java/olx/rent/bot/olx/UpdateScheduler.java
@@ -2,26 +2,23 @@ package olx.rent.bot.olx;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @Slf4j
 public class UpdateScheduler {
 
     private final TelegramService telegramService;
-    private final OlxPostService olxPostService;
     private final ChatSettingsDao chatSettingsDao;
 
     public UpdateScheduler(TelegramService telegramService,
-                           OlxPostService olxPostService,
                            ChatSettingsDao chatSettingsDao) {
         this.telegramService = telegramService;
-        this.olxPostService = olxPostService;
         this.chatSettingsDao = chatSettingsDao;
     }
 
 
-    @Scheduled(cron = "0 0/15 * * * *")
+    @Scheduled(cron = "0 0/20 * * * *")
     public void checkUpdatesAndSendIfNewOnesAre() {
         //todo add pagination in case performance issues
         log.info("planned posts updating started");
